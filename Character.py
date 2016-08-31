f5080657d69699d9d29c5f9f96a4bdb6c63cd0b7
Bin Chen <chenbin11200+git@gmail.com>
2016-08-31 17:36:04 +0200

    precise walk

--- Character.py
+++ Character.py
@@ -1,6 +1,7 @@
 import decimal
 import math
 import Helper
+import GPSLocationPointCreatetor
 
 
 class Character:
@@ -12,26 +13,34 @@ class Character:
         self.routeMemory = []
 
     def moveTo(self, targetLocationPoint):
-        x1 = self.currentLocationPoint['lat']
-        y1 = self.currentLocationPoint['lng']
-        x2 = self.targetLocationPoint['lat']
-        y2 = self.targetLocationPoint['lng']
-        lastLat = decimal.Decimal(x1)
-        lastLng = decimal.Decimal(y1)
-        a = y1 - y2
-        b = x1 - x2
-        c = math.sqrt(a * a + b * b)
-        sinAngle = a / c
-        cosAngle = b / c
-        distance = decimal.Decimal(Character.WalkSpeed * 0.000001)
-        counter = 0
-        while counter < c:
-            lastLat = lastLat - distance * decimal.Decimal(cosAngle)
-            lastLng = lastLng - distance * decimal.Decimal(sinAngle)
-            counter = counter + distance
-            Helper.updateGPSLocation(lastLat, lastLng)
-        Helper.update_gps_location(lastLat, lastLng)
-        print("clicking to endpoint!!")
+        """
+            x1 = self.currentLocationPoint['lat']
+            y1 = self.currentLocationPoint['lng']
+            x2 = self.targetLocationPoint['lat']
+            y2 = self.targetLocationPoint['lng']
+        """
+        latRange = range(min(self.currentLocationPoint['lat'], targetLocationPoint['lat']),
+                         max(self.currentLocationPoint['lat'], targetLocationPoint['lat']))
+        lngRange = range(min(self.currentLocationPoint['lng'], targetLocationPoint['lng']),
+                         max(self.currentLocationPoint['lng'], targetLocationPoint['lng']))
+
+        pathLineRad = GPSLocationPointCreatetor.getPathLineRad(self.currentLocationPoint, self.targetLocationPoint)
+        distanceAtLat = GPSLocationPointCreatetor.getDistanceInLat(self.currentLocationPoint['lat'],
+                                                                   Character.WalkSpeed)
+        differenceInLat = GPSLocationPointCreatetor.getDistanceInLat(self.currentLocationPoint['lat'],
+                                                                     Character.WalkSpeed)['lat']
+
+        realChangeInLat = (differenceInLat/distanceAtLat)*Character.WalkSpeed
+        realChangeInLng = realChangeInLat*math.tan(pathLineRad)
+
+        while self.currentLocationPoint['lat'] in latRange and self.currentLocationPoint['lng'] in lngRange:
+            self.currentLocationPoint['lat'] += realChangeInLat
+            self.currentLocationPoint['lng'] += realChangeInLng
+            Helper.updateGPSLocation(self.currentLocationPoint['lat'], self.currentLocationPoint['lng'])
+            self.recordInMemory()
+        Helper.update_gps_location(targetLocationPoint['lat'], targetLocationPoint['lng'])
+        self.recordInMemory()
+        print("Reach and point, wait to start new trip.")
         return
 
     def recordInMemory(self):
--- GPSLocationPointCreatetor.py
+++ GPSLocationPointCreatetor.py
@@ -1,6 +1,7 @@
 import xml.etree.cElementTree as ET
 import math
 
+
 def createGPSFile4IOS(lat, lng):
     gpx = ET.Element("gpx", version="1.1", creator="Xcode")
     wpt = ET.SubElement(gpx, "wpt", lat="%.6f" % lat, lon="%.6f" % lng)
@@ -12,6 +13,11 @@ def rad(angle):
     return angle * math.pi / 180
 
 
+# http://www.cnblogs.com/zrhai/p/3817492.html
+# http://www.ucbbs.org/cgi-bin/bbs/ccb/topic_view.cgi?forum=1&article_id=0101080324231941&class=1
+# Calculate max latitude & longitude difference according to a certain latitude and distance(in meter)
+# The result is a square area
+# Distance in meter
 def getMaxDeviation(lat, distance):
     EARTH_RADIUS = 6377.830  # in km
     radLat = rad(lat)
@@ -26,3 +32,13 @@ def getDistanceInLat(lat, distance):
     lat = getMaxDeviation(lat, distance)["lat"]
     lng = getMaxDeviation(lat, distance)["lng"]
     return math.sqrt(lat*lat + lng*lng)
+
+
+def getPathLineRad(startLocationPoint, destinationLocationPoint):
+    x1 = startLocationPoint['lat']
+    y1 = startLocationPoint['lng']
+    x2 = destinationLocationPoint['lat']
+    y2 = destinationLocationPoint['lng']
+    return math.atan2(y2-y1, x2-x1)
+
+
