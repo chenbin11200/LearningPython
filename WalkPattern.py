7ffb793d94b7c3a55eed9efe5d28e0626393eb8f
Bin Chen <chenbin11200+git@gmail.com>
2016-09-05 16:06:53 +0200

    use external file to save private key

--- WalkPattern.py
+++ WalkPattern.py
@@ -2,12 +2,15 @@ import urllib2
 import json
 
 class WalkPattern:
+
     def __init__(self, trainer, gobackAfterArrive = True):
-        self.description = "Wakl by Google map"
+        self.description = "Walk by Google map"
         self.origin = str(trainer.currentLocationPoint["lat"])+","+str(trainer.currentLocationPoint["lng"])
         self.destination = str(trainer.targetLocationPoint["lat"])+","+str(trainer.targetLocationPoint["lng"])
         self.trainer = trainer
         self.gobackAfterArrive = gobackAfterArrive
+        self.googleApiKey = self.__readGoogleKey()
+        self.a = 'a'
 
 
     def start(self):
@@ -24,7 +27,7 @@ class WalkPattern:
         try:
             response = urllib2.urlopen(
                 "https://maps.googleapis.com/maps/api/directions/json?origin=" + self.origin + "&destination="
-                + self.destination + "&mode=walking&key=", timeout=3)
+                + self.destination + "&mode=walking&key="+self.googleApiKey, timeout=3)
             route = json.load(response)
 
             for item in route["routes"][0]["legs"][0]["steps"]:
@@ -32,4 +35,9 @@ class WalkPattern:
                 self.trainer.moveTo(item['end_location'])
         except urllib2.URLError as e:
             print(e.reason)
+
+
+    def __readGoogleKey(self):
+        key = open("GoogleApiKey.ppk", 'r').read()
+        return key
 
